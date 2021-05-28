8a255f1e545491524ebaaae8081f20a32a34ab79
pierre villard <pvillard@ilki.fr>
2021-05-28 13:34:51 +0200

    Update aks-inject-ssh-key.sh

--- aks-inject-ssh-key.sh
+++ aks-inject-ssh-key.sh
@@ -25,7 +25,7 @@ az vmss extension set \
   --name VMAccessForLinux \
   --publisher Microsoft.OSTCExtensions \
   --version 1.4 \
-  --protected-settings "{\"username\":\"azureuser\", \"ssh_key\":\"$(cat ~/.ssh/id_rsa.pub)\"}"
+  --protected-settings "{\"username\":\"azureuser\", \"ssh_key\":\"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCs/Mde8ivvzJo0AeqKywxhY3ObA2OK62ahvoDd7vm6EgQ61XbvlYv4Mlvac1OvYz/MsM9wRs3kHY88xRkdYFgKQelT4weXekkxpuecKKFYy/6L2Rojm1OFX+C9pibiDsht4E0mfhmkC3TLfmQHVkNbiBMhv7tWakGKxaPWhqbZXD0a+Dz4zMTphHG5ayUocIZvlYTkwmXacA9ZG8Gz7u7xjcE7e8bbe2rdbhlnhX/nOed8zsxNQCO7ixH90IfX/sxa5vCbYSFGrZqJPGX/I2+xFqdgdby6WhhV8rxmVlMvGvG5M1SRWxsHU94KZ8tEFuJdAPPfm6pXk5YMvLX39fF0lHjXa7cQAefb/utEqci5nPlUaGrTlDF6xyuuDR2p3L90PfR4CwIRQ6WdX9V0prx+cPdQPDQans0EQW03JkOwlOgeQBHRSZG+xWU9y+WcpxDd7Eu5yR5cgDJ1OP7u23lI5TtShuNx7eZEVJIPY9C79WtqfBgsdLsvdKa7KuDxNP8=\"}"
 
 az vmss update-instances --instance-ids '*' \
     --resource-group $CLUSTER_RESOURCE_GROUP \
